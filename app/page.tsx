0af6f8b604750327d4fdeecc10abb6446528b5e2
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-07-03 17:53:21 -0600

    sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 export default function Home() {
   return (
     <main>
-      <div className="text-center">  {/*Div de titulo */}
-        <br />
-        <h1 className="font-bold text-7xl text-[#2C4277] ">Milo</h1>
-        <h2 className="font-light text-2xl italic text-[#98A8C4]">La clave para un análisis eficiente</h2>
+      <div className=" w-full flex items-center border-b-[1px] sm:px-4 py-3 px-4 lg:px-6 justify-between shadow-sm">  {/*Div de titulo */}
+        
+        <h1 className="font-bold text-7xl text-[#2C4277] text-center">Milo</h1>
+        <h2 className="font-light text-2xl italic text-[#98A8C4] text-center">La clave para un análisis eficiente</h2>
       </div> {/*Fin Div de titulo */}
     </main>
   );
