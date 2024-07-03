aca34c1d10bf9c0029139ced1282905638d3ba4b
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-07-03 17:16:25 -0600

    div de titulo

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@
 export default function Home() {
   return (
     <main>
-      <div>
-        <p>Ask Milo!</p>
-      </div>
+      <div className="text-center">  {/*Div de titulo */}
+        <br />
+        <h1 className="font-bold text-7xl text-[#2C4277] ">Milo</h1>
+        <h2 className="font-light text-2xl italic text-[#98A8C4]">La clave para un análisis eficiente</h2>
+      </div> {/*Fin Div de titulo */}
     </main>
   );
 }
