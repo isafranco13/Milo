13980cf6256db1ac30b3a3aeff32fca6b5e7c9f5
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-07-03 21:49:34 -0600

    sidebar creation

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,4 @@
+const EmptyState = () => {
+
+}
+export default EmptyState;
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,15 @@ export default function Home() {
   return (
     <main>
       <div className=" w-full flex items-center border-b-[1px] sm:px-4 py-3 px-4 lg:px-6 justify-between shadow-sm">  {/*Div de titulo */}
-        <h1 className="font-bold text-[70px] text-[#2C4277] text-center">Milo</h1>
-        <Image src="/MiloV1r.png" alt="Milo" width={100} height={100} />
+        <div className="flex justify-normal"> {/*Div logo y nombre */}
+          <Image src="/MiloV1r.png" alt="Milo" width={100} height={100} /> &nbsp;&nbsp;&nbsp;
+          <h1 className="font-bold text-[70px] text-[#2C4277] text-center">Milo</h1>
+        </div> {/*Fin Div logo y nombre */}
         <h2 className="font-semibold text-[20px] italic text-[#98A8C4] text-center">La clave para un análisis eficiente</h2>
       </div> {/*Fin Div de titulo */}
+      <div className="hidden lg:block lg:pl-80 h-full">
+
+      </div>
     </main>
   );
 }
