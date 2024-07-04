6a5d9df1798b6177f6b34ee42069a825a7aa3d3d
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-07-03 18:24:09 -0600

    milo sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
       <div className=" w-full flex items-center border-b-[1px] sm:px-4 py-3 px-4 lg:px-6 justify-between shadow-sm">  {/*Div de titulo */}
-        
-        <h1 className="font-bold text-7xl text-[#2C4277] text-center">Milo</h1>
-        <h2 className="font-light text-2xl italic text-[#98A8C4] text-center">La clave para un análisis eficiente</h2>
+        <h1 className="font-bold text-[70px] text-[#2C4277] text-center">Milo</h1>
+        <Image src="/MiloV1r.png" alt="Milo" width={100} height={100} />
+        <h2 className="font-semibold text-[20px] italic text-[#98A8C4] text-center">La clave para un análisis eficiente</h2>
       </div> {/*Fin Div de titulo */}
     </main>
   );
