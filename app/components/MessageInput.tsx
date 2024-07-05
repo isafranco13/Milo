800f4c124b19436a951862e9392ab9f15aa20c5f
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-07-04 18:44:22 -0600

    MessageInput style

--- app/components/MessageInput.tsx
+++ app/components/MessageInput.tsx
@@ -14,7 +14,8 @@ interface MessageInputProps {
 const MessageInput: React.FC<MessageInputProps> = ({placeholder,id, type, required, register, errors}) => {
     return(
         <div className="relative w-full ">
-            <input id={id} type={type} autoComplete={id} {...register(id, {required})} />
+            <input id={id} type={type} autoComplete={id} {...register(id, {required})} placeholder={placeholder}
+            className="text-black font-light py-2 px-4 bg-neutral-100 w-full rounded-full focus:outline-none"/>
         </div>
     );
 };
