baa612b32a8bf7deec8a7ab2c169b8f6d52bc898
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-07-17 16:42:18 -0600

    creacion chatid dinamico

---
+++ app/[chatId]/page.tsx
@@ -0,0 +1,11 @@
+//import EmptyState from './components/EmptyState';
+//import Historial from './components/Historial';
+
+export default function Chat() {
+    return (
+        <div>
+            Chat
+        </div>
+    )
+
+}
