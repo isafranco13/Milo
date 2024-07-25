0cb8ae3571502c9f60fa40365875d4a8983a6d06
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-07-24 18:06:55 -0600

    sendMessage route created

---
+++ app/api/chat/sendMessage.js
@@ -0,0 +1,11 @@
+export const config = {
+ runtime: "edge",
+};
+
+export async function handler(req){
+    try{
+    const {message} = req.json;
+    }catch(e){
+        console.log("Un error ha ocurrido al enviar el mensaje", e);
+    }
+}
