e729d8b9e578f1d35d489b9068cd99ebbfcfd43e
isafranco13 <beatrizfrancodiaz@gmail.com>
2024-08-01 21:01:46 -0600

    delete components usless

--- app/components/Body.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-const Body = () => {
-    return(
-        <div className="flex-1 overflow-y-auto">
-            Body!</div>
-    );
-}
-export default Body;
--- app/components/Message/Message.js
+++
@@ -1,7 +0,0 @@
-export const Message = () => {
-    return (
-        <div>
-            <h1>Message</h1>
-        </div>
-    )
-}
